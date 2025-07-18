49258202bca91565501c63cecb3842d9296b4097
bingo7894 <62197197+bingo7894@users.noreply.github.com>
2025-07-18 18:12:19 +0700

    Update  vercel nuxt.config.ts
    แก้ vercel

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// nuxt.config.ts
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
@@ -14,13 +13,42 @@ export default defineNuxtConfig({
     githubSecret: "",
   },
 
-  // 👇 เพิ่มส่วนนี้
   nitro: {
     handlers: [
       {
-        route: '/api/auth/login',
+        route: '/api/auth/github',
+        method: 'get',
+        handler: '~/server/api/auth/github.get.ts',
+      },
+      {
+        route: '/api/auth/register',
         method: 'post',
-        handler: '~/server/api/auth/login.post.ts',
+        handler: '~/server/api/auth/register.post.ts',
+      },
+      {
+        route: '/api/account/stats',
+        method: 'get',
+        handler: '~/server/api/account/stats.get.ts',
+      },
+      {
+        route: '/api/history/:id',
+        method: 'get',
+        handler: '~/server/api/history/[id].get.ts',
+      },
+      {
+        route: '/api/history',
+        method: 'get',
+        handler: '~/server/api/history/index.get.ts',
+      },
+      {
+        route: '/api/history',
+        method: 'post',
+        handler: '~/server/api/history/index.post.ts',
+      },
+      {
+        route: '/api/report',
+        method: 'post',
+        handler: '~/server/api/report/index.post.ts',
       },
     ],
   },
